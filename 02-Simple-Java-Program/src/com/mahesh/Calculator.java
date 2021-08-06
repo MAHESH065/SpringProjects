44cefa2208c4b784b7f1c1a9f79e330312f90bf9
Mahesh <maheshwar.4ever@gmail.com>
2021-08-06 08:13:55 +0530

    first commit

---
+++ 02-Simple-Java-Program/src/com/mahesh/Calculator.java
@@ -0,0 +1,15 @@
+package com.mahesh;
+
+public class Calculator {
+	
+	public void add(int a, int b) {
+		int result = a+b;
+		System.out.println("Add :: " + result);
+	}
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		cal.add(10, 20);
+	}
+
+}
