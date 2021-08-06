1f607905ce2f07736ed912cd14bb127f4cb181be
Mahesh <maheshwar.4ever@gmail.com>
2021-08-06 08:42:02 +0530

    sc

--- 02-Simple-Java-Program/src/com/mahesh/Calculator.java
+++ 02-Simple-Java-Program/src/com/mahesh/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public void add(int a, int b) {
 		int result = a+b;
 		System.out.println("Add :=" + result);
+		System.out.println("Second commit");
 	}
 
 	public static void main(String[] args) {
